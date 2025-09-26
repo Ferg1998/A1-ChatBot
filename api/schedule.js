9449e87a8c272c954eab2466e24a78ca3c421e07
Ferg1998 <fergiegoalie@yahoo.com>
2025-09-26 11:34:42 -0400

    Create schedule.js

---
+++ api/schedule.js
@@ -0,0 +1,31 @@
+// schedule.js
+const A1_SCHEDULE = {
+  monday: [
+    { name: "Sweat & Sculpt", time: "7:00 AM", length: "45 min" },
+    { name: "Sweat & Sculpt", time: "5:00 PM", length: "45 min" }
+  ],
+  tuesday: [
+    { name: "Sweat & Sculpt", time: "5:30 PM", length: "45 min" },
+    { name: "Sweat & Sculpt", time: "6:30 PM", length: "45 min" }
+  ],
+  wednesday: [
+    { name: "Sweat & Sculpt", time: "7:00 AM", length: "45 min" },
+    { name: "Sweat & Sculpt", time: "12:00 PM", length: "45 min" }
+  ],
+  thursday: [
+    { name: "Sweat & Sculpt", time: "5:30 PM", length: "45 min" },
+    { name: "Sweat & Sculpt", time: "6:30 PM", length: "45 min" }
+  ],
+  friday: [
+    { name: "Sweat & Sculpt", time: "5:00 AM", length: "45 min" }
+  ],
+  saturday: [
+    { name: "Sweat & Sculpt", time: "9:00 AM", length: "45 min" },
+    { name: "Sweat & Sculpt", time: "10:00 AM", length: "45 min" }
+  ],
+  sunday: [
+    { name: "Sweat & Sculpt", time: "12:00 PM", length: "45 min" }
+  ]
+};
+
+export default A1_SCHEDULE;
